6c496d315bf358d13423ceda686f4ed8d7f52664
olvchae <eunchae1405@gmail.com>
2026-04-03 17:48:23 +0900

    0403

--- src/main/java/study/ch16/Static02.java
+++ src/main/java/study/ch16/Static02.java
@@ -3,6 +3,10 @@ package study.ch16;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 
@@ -10,8 +14,6 @@ class User{
     private Long id;
     private String username;
     private String password;
-
-
 }
 
 class UserRepository{
@@ -22,7 +24,7 @@ class UserRepository{
         users = new User[1000];
     }
 
-    public void addUser(User user){
+    public boolean addUser(User user){
         boolean isSuccess = false;
         for(int i = 0; i < users.length; i++){
             if (users[i] == null){
@@ -33,24 +35,90 @@ class UserRepository{
             }
             if (!isSuccess){
                 System.out.println("사용자 추가 실패");
-                return;
+                return isSuccess;
             }
             System.out.println("사용자 추가 성공");
             System.out.println("추가된 사용자 정보:" + user);
+        }
+        return isSuccess;
+    }
+
+    public User findByUsername(String username){
+        for(User user : users){
+            if(user == null) continue;
+            if(user.getUsername().equals(username)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void printUsers(){
+        System.out.println("사용자 전체 조회");
+        for(User user : users){
+            System.out.println(user);
         }
     }
 }
 
 class UserService{
+    public int signup(String username, String password){
+        // 성공: 200 실패: 400(중복 아이디), 500(공간부족)
+        int status = 200;
 
+        UserRepository userRepository = new UserRepository();
+        User foundUser = userRepository.findByUsername(username);
+        if(foundUser != null){
+            return 400;
+        }
+
+        User newUser = new User(0l, username, password);
+//        boolean isSuccess = UserRepository.addUser(username);
+        userRepository.addUser(newUser);
+        return status;
+
+    }
 }
 
 class UserController{
-
+    public void postMapping(String username, String password){
+        UserService userService = new UserService();
+        int status = userService.signup(username, password);
+        switch (status){
+            case 200:
+                System.out.println("회원 등록 성공");
+                break;
+            case 400:
+                System.out.println("중복된 사용자 이름입니다.");
+                break;
+            case 500:
+                System.out.println("데이터를 더이상 추가할 수 없습니다.");
+                break;
+        }
+    }
 }
 
 public class Static02 {
     public static void main(String[] args) {
+        String[] usernames = new String[500];
+        String[] password = new String[500];
+        for (int i = 0; i < 500; i++){
+            Random random = new Random();
+            usernames[i] = "";
+            for(int j = 0; j < 10; j++){
+                int r = random.nextInt(26) + 97;
+                usernames[i] += (char)r;
+            }
+            usernames[i] += "@gmail.com";
+            password[i] = UUID.randomUUID().toString().replaceAll("-", "");
+        }
 
+        System.out.println(Arrays.toString(usernames));
+        System.out.println(Arrays.toString(password));
+
+        UserController userController = new UserController();
+        for(int i = 0; i < 500; i++){
+            userController.postMapping(usernames[i], password[i]);
+        }
     }
 }
